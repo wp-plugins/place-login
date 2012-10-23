b45c99e73003b984816758835f86b502d659d046
lebasca <lebasca@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-23 22:34:20 +0000

    git-svn-id: https://plugins.svn.wordpress.org/place-login/trunk@616560 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/plogin_register.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<div id="container">
-	<div id="content" role="main">
-		<?php plogin_register_form() ?>
-	</div><!-- #content -->
-</div><!-- #container -->
-
-<?php get_footer(); ?>
