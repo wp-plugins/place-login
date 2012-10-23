3c7633ccabdf6f79ff94e503e3b811b3759cf650
lebasca <lebasca@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-23 15:22:39 +0000

    git-svn-id: https://plugins.svn.wordpress.org/place-login/trunk@616335 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/plogin_lostpwd.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<div id="container">
+	<div id="content" role="main">
+		<?php plogin_lostpwd_form() ?>
+	</div><!-- #content -->
+</div><!-- #container -->
+
+<?php get_footer(); ?>
---
+++ templates/plogin_profile.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<div id="container">
+	<div id="content" role="main">
+		<?php plogin_profile_form(); ?>
+	</div><!-- #content -->
+</div><!-- #container -->
+
+<?php get_footer(); ?>
