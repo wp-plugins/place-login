af79fb1c601d198129cc9ce67275dfeac28a922a
lebasca <lebasca@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-23 22:42:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/place-login/trunk@616575 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/plogin-menu.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+	jQuery('.display-user').hover(function(){                
+		//display the submenu
+		jQuery('.display-user #dropdown').toggle();
+	});
+
+	jQuery('.plogin_widget').hover(function(){                 
+		//display the submenu
+		jQuery('.plogin_widget #dropdown').toggle();
+	});
+});
